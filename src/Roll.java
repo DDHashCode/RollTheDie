bf0d2253579121e362efd8c7c83617d4ee435bc9
DDHashCode <30550195+ddhashcode@users.noreply.github.com>
2018-07-18 11:41:38 +0200

    Basic version fully working.

--- src/Roll.java
+++ src/Roll.java
@@ -1,18 +1,40 @@
+import java.util.InputMismatchException;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Roll {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InputMismatchException {
        Scanner input = new Scanner(System.in);
-        Pattern pat = Pattern.compile("\\d\\dd\\d\\d");
-        System.out.println(pat.matcher("1d3").find());
+       String txt = getInput(input);
+       if ( txt.contains("d")){
+           System.out.println("Wynik rzutów kostką: " + compute(txt));
 
-       //System.out.println("You have provided: " + getInput(input));
+       }
+       else
+           throw new InputMismatchException("You input " + txt + " not contains 'd' letter.");
+
+
+
+    }
+    private static int compute(String inputieren){
+        String[] tabz = inputieren.split("d");
+        String dieAmount = tabz[0];
+        String dieSides = tabz[1];
+        int dices = Integer.parseInt(dieAmount);
+        int sides = Integer.parseInt(dieSides);
+        Random generator = new Random();
+        int sum = 0;
+        for (int i=0; i<dices; i++){
+            sum += generator.nextInt(sides);
+        }
+
+        return sum;
 
     }
 
-    public static String getInput(Scanner in){
+    private static String getInput(Scanner in){
         System.out.println("Please provide your input:");
         return in.next();
     }
